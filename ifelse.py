9858f317d20cb86857a8a3d64cea3cfde6ddffdb
JeongTae <wjdxo513@naver.com>
2023-07-25 11:21:56 +0900

    3장 완료

--- ifelse.py
+++ ifelse.py
@@ -50,7 +50,7 @@ print("---리스트 컴프리헨션---")
 lst = list(range(1,11))
 print( [i**2 for i in lst if i > 5])
 tp = ("apple", "banana", "orange")
-print( [len(i) for i in tp])
+print( [len(i) for i in tp] ) 
 d = {100:"apple", 200:"kiwi"}
 print( [v.upper() for v in d.values()] )
 
